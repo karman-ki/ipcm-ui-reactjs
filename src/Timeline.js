2d50500781fa321a905c47da4a26c69b96371124
Solbritt Gateman <solbritt@gateman.se>
2022-10-26 15:53:01 +0200

    Updated timeline colors

--- src/Timeline.js
+++ src/Timeline.js
@@ -16,20 +16,36 @@ const Timeline = ({ step }) => {
 
     const colors = ['', '#f5a551', '#e8c64d', '#d6d63c', '#c4e046', '#a8de45', '#96e046', '#71db44', '#3bd13b']
 
+    const ActiveNode = (step) => {
+        return (
+            <div className="node active" style={{ backgroundColor: colors[step] }}></div>
+        )
+    }
+    const ActivePath = (step) => {
+        return (
+            <div className="path active" style={{ background: `linear-gradient(90deg, ${colors[step]} 0%, ${colors[step + 1]} 100%)` }}></div>
+        )
+    }
+    const SemiActivePath = (step) => {
+        return (
+            <div className="path semi-active" style={{ background: `linear-gradient(90deg, ${colors[step]} 50%, rgb(196, 196, 196) 50%)` }}></div>
+        )
+    }
+
     const stepNum = stepMap[step];
     let steps = []
     const len = Object.keys(stepMap).length
     for (let i = 1; i < len; i++) {
 
         if (i < stepNum) {
-            steps.push(<div className="node active" style={{ backgroundColor: colors[i] }}></div>)
-            steps.push(<div className="path active" style={{ background: `linear-gradient(90deg, ${colors[i]} 0%, ${colors[i+1]} 100%)` }}></div>)
+            steps.push(ActiveNode(i));
+            steps.push(ActivePath(i))
         } else if (i == stepNum) {
             if (i < len - 1) {
-                steps.push(<div className="node active" style={{ backgroundColor: colors[i] }}></div>)
-                steps.push(<div className="path semi-active" style={{ background: `linear-gradient(90deg, ${colors[i]} 50%, rgb(196, 196, 196) 50%)` }}></div>)
+                steps.push(ActiveNode(i))
+                steps.push(SemiActivePath(i))
             } else {
-                steps.push(<div className="node active" style={{ backgroundColor: colors[i] }}></div>)
+                steps.push(ActiveNode(i))
             }
         } else {
             if (i < len - 1) {
