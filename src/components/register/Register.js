d61cd8f6bbe2d78f2ce67e3dd8284486e044561a
Sarayu4995 <sarayuthathathri.ki@gmail.com>
2023-01-03 09:43:59 +0100

    CSS changes

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,7 +1,8 @@
-import React, {useState } from "react";
+import React, {useState, useEffect } from "react";
 import { Formik, Field, Form, ErrorMessage } from 'formik';
+import Select from 'react-select';
 import * as Yup from 'yup';
-import { useNavigate, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 
 import "react-toastify/dist/ReactToastify.css";
@@ -11,26 +12,19 @@ import './Register.css';
 import BrandLogo from '../../assets/images/logo/logo.png';
 import AuthService from "../../services/authService";
 
-
-
-const siteInfo = [];	
-siteInfo.push(<option key={""} value={""}>{"-- Select option --"}</option>);
-
-	AuthService.siteList().then(
-		(data) =>{
-			const resultSet=data['data'];				
-			for (let i = 0; i < resultSet.length; i++)  {								
-				siteInfo.push(<option key={resultSet[i].site_id} value={resultSet[i].site_id}>{resultSet[i].site_name}</option>);	
-			}
-		}
-	) 
-	
 function Register() {
 
-	const navigate = useNavigate();
+	const [siteOption, setSiteOption] = useState(null);
+	const [showSite, setShowSite] = useState(false);
 
+	const roleOption = [
+		{ value: "Pathologist", label: "Pathologist" },
+		{ value: "Doctor", label: "Doctor" },
+		{ value: "Nurse", label: "Nurse" }
+	];
+	
 	//Field validation using Yup hook
-		const validationSchema = Yup.object().shape({
+	const validationSchema = Yup.object().shape({
 		emailID: Yup.string()
 			.required('Email is required')
 			.email('Email is invalid'),
@@ -42,21 +36,21 @@ function Register() {
 			.matches(/^(?=.*[A-Z])/, 'Must contain at least one uppercase character')
 			.matches(/^(?=.*[0-9])/, 'Must contain at least one number')
 			.matches(/^(?=.*[!@#%&])/, 'Must contain at least one special character'),
-		firstname: Yup.string()
+		firstName: Yup.string()
 			.min(4, 'Too Short!')
 			.max(50, 'Too Long!')
 			.required('First Name is required'),
-		lastname: Yup.string()
+		lastName: Yup.string()
 			.min(4, 'Too Short!')
 			.max(50, 'Too Long!')
 			.required('Last Name is required'),
 		confirmpassword: Yup.string()
 			.required('Please retype your password.')
 			.oneOf([Yup.ref('password')], 'Your passwords do not match.'),
-		role: Yup.string()
+		role:Yup.string()
 			.required('Role is required'),
-		site: Yup.string()
-			.required('Site is Required'),
+		site:Yup.string()
+			.required('Site is required'),
 		acceptTerms: Yup.bool().oneOf([true], 'Accept Terms & Conditions is required'),
 			
 		});
@@ -71,39 +65,23 @@ function Register() {
 	const initialValues = {
 		emailID: '',
 		password: '',
-		firstname: '',
-		lastname: '',
+		firstName: '',
+		lastName: '',
 		confirmpassword: '',
-		role:'',
+		role: '',
 		site:'',
 		acceptTerms: false,
-	  };
+	};
 
-	  const [showSite, setShowSite] = useState(false);
-	  const [roleVal, setRoleVal] = useState('');
-	  
-	  const handleChange = (event) => {
-		const value = event.target.value;
-		if(value !== "" && value !== "Pathologist"){
-			setShowSite(true);
-		}
-		else{
-			setShowSite(false);
-		}
-		setRoleVal(value)
-	  };
-	  
-	 const handleSubmit = (data) => {
+	const handleSubmit = (data, {resetForm}) => {
+
 		let params = {};
-		// const siteName=data["site"]
 		params["email_id"] = data["emailID"];
 		params["pwd"] = data["password"];
-		params["first_name"] = data["firstname"];
-		params["last_name"] = data["lastname"];
+		params["first_name"] = data["firstName"];
+		params["last_name"] = data["lastName"];
 		params["role"]= data["role"];
-		params["site"]= data["site"];
-		
-		
+		params["site"]= ( data["site"] === '-' ? '': data["site"]);
 		AuthService.userRegisteration(params).then(
 			(response) => {
 				if(response['status']){
@@ -111,7 +89,7 @@ function Register() {
 					toast.success(msg, {
 						position: toast.POSITION.TOP_RIGHT
 					});
-					navigate("/login");							
+					resetForm({})
 				}else{
 					const msg = response['message'];
 					toast.error(msg, {
@@ -123,110 +101,166 @@ function Register() {
 					position: toast.POSITION.TOP_RIGHT
 				});
 			
-			})			
+		})			
 	}
-			
 
-    return (
+	useEffect(() => {	
+		const siteInfo = []
+		AuthService.siteList().then(
+			(data) =>{
+				const resultSet=data['data'];				
+				for (let i = 0; i < resultSet.length; i++)  {								
+					siteInfo.push({"label": resultSet[i].site_name, "value" :resultSet[i].site_id})
+				}
+				setSiteOption(siteInfo);
+			}
+		) 
+	},[])
+
+	return (
 		<>
-        <div className="container-register100">
-			<div className="row wrap-register100">
-				<div className="col-5 pt-5">
-					<img className="register100-pic" src={BrandLogo} alt="iPCM logo"/>
-                    <h2 className="register-app-title"> iPCM leaderboard</h2>
-					<div className="form-group text-left pt-1">
-						Already have an account? <a href="/login" className="link-primary auth-link"> LogIn </a>
+			<div className="container-register100">
+				<div className="row wrap-register100">
+					<div className="col-5 pt-5">
+						<img className="register100-pic" src={BrandLogo} alt="iPCM logo"/>
+						<h2 className="register-app-title"> iPCM leaderboard</h2>
+						<div className="form-group text-left pt-1">
+							Already have an account? <a href="/login" className="link-primary auth-link"> LogIn </a>
+						</div>
+					</div>
+					<div className="col-6 pt-2">
+						<Formik
+							initialValues={initialValues}
+							validationSchema={validationSchema}
+							onSubmit={handleSubmit}
+							>
+							{({ values, errors, touched, setFieldValue, resetForm, props }) => (
+								<Form className="register100-form validate-form" id="signupForm">
+									<h4 className="register100-form-title font-weight-bold">Register a new account</h4>
+									<div className="form-row">
+										<div className="form-group col-6">
+												<label htmlFor="firstName" className="mandatory"> First Name </label>
+												<Field
+													id="firstName"
+													name="firstName" 
+													placeholder="First Name"
+													type="text"
+													className={ 'form-control' + (errors.firstName && touched.firstName ? ' is-invalid' : '') }
+												/>
+												<ErrorMessage name="firstName" component="div" className="invalid-feedback" />
+										</div>
+										<div className="form-group col-6">
+												<label htmlFor="lastName" className="mandatory"> Last Name </label>
+												<Field
+													id="lastName"
+													name="lastName"
+													placeholder="Last Name"
+													type="text"
+													className={ 'form-control' + (errors.lastName && touched.lastName ? ' is-invalid' : '') }
+												/>
+												<ErrorMessage name="lastName" component="div" className="invalid-feedback" />
+										</div>
+									</div>	
+									<div className="form-group">
+										<label htmlFor="emailID" className="mandatory"> Email </label>
+										<Field 
+											name="emailID" 
+											type="email"
+											placeholder="Email-Id"
+											className={ 'form-control' + (errors.emailID && touched.emailID ? ' is-invalid' : '') }
+										/>
+										<ErrorMessage name="emailID" component="div" className="invalid-feedback" />
+									</div>
+									<div className="form-group">
+										<label htmlFor="role" className="mandatory">Role</label> 
+										<Select
+											name="role"
+											options={roleOption}
+											placeholder="-- Select Role Option --"
+											onChange={(option) => {
+												const opt_val = option.value
+												setFieldValue("role", opt_val);
+												if(opt_val !== "" && opt_val !== "Pathologist"){
+													setShowSite(true);
+													setFieldValue("site", "");
+												} else{
+													setShowSite(false);
+													setFieldValue("site", "-");
+												}
+											}}
+											className={ '' + (errors.role && touched.role ? ' is-invalid' : '') }
+										/>
+										<ErrorMessage name="role" component="div" className="invalid-feedback" />
+									</div>
+									{showSite && (
+									<div className="form-group">
+										<label htmlFor="site" className="mandatory">Site</label>
+										<Select
+											name="site"
+											options={siteOption}
+											placeholder="-- Select Site Option --"
+											onChange={(option) => {
+												setFieldValue("site", option.value);
+											}}
+											className={ '' + (errors.site && touched.site ? ' is-invalid' : '') }
+										/>
+										<ErrorMessage name="site" component="div" className="invalid-feedback" />
+									</div>
+									)}
+									<div className="form-group">
+										<label htmlFor="password" className="mandatory"> Password </label>
+										<Field 
+											name="password" 
+											type="password" 
+											className={ 'form-control' + (errors.password && touched.password ? ' is-invalid' : '') }
+											placeholder="Password"
+										/>
+										<ErrorMessage name="password" component="div" className="invalid-feedback" />
+									</div>
+									<div className="form-group">
+										<label htmlFor="confirmpassword" className="mandatory"> Confirm Password </label>
+										<Field 
+											name="confirmpassword" 
+											type="password" 
+											className={ 'form-control' + (errors.confirmpassword && touched.confirmpassword ? ' is-invalid' : '') }
+											placeholder="Confirm Password"
+										/>
+										<ErrorMessage name="confirmpassword" component="div" className="invalid-feedback" />
+									</div>
+									<div className="form-group pt-2">
+										<div className="custom-checkbox custom-control">
+											<Field type="checkbox" name="acceptTerms" className={'form-check-input ' + (errors.acceptTerms && touched.acceptTerms ? ' is-invalid' : '')} />
+											<label htmlFor="acceptTerms" className="form-check-label">I agree to the <a href="termsandconditions.html">Terms and Conditions </a></label>
+											<ErrorMessage name="acceptTerms" component="div" className="invalid-feedback" />
+										</div>
+									</div>
+									<div className="form-group pt-3 pb-3">
+										<button type="submit" className="btn btn-primary btn-block" id="register">
+											Register
+										</button>
+									</div>
+								</Form>
+							)}
+						</Formik>
+					</div>
+					<div className=" col-12 footer">
+						<strong>iPCM Leaderboard</strong> &copy;2022, All rights reserved.
 					</div>
 				</div>
-				<div className="col-6 pt-2">
-					<Formik
-						initialValues={initialValues}
-						validationSchema={validationSchema}
-						onSubmit={handleSubmit}
-						>
-						{({ errors, touched, resetForm, props }) => (
-							<Form className="register100-form validate-form" id="signupForm">
-								<h4 className="register100-form-title font-weight-bold">Register a new account</h4>
-								<div className="form-row">
-									<div className="form-group col-6">
-											<label htmlFor="firstname" className="mandatory"> First Name </label>
-											<Field name="firstname" type="firstname" className={ 'form-control' + (errors.firstname && touched.firstname ? ' is-invalid' : '') }/>
-											<ErrorMessage name="firstname" component="div" className="invalid-feedback" />
-									</div>
-									<div className="form-group col-6">
-											<label htmlFor="lastname" className="mandatory"> Last Name </label>
-											<Field name="lastname" type="lastname" className={ 'form-control' + (errors.lastname && touched.lastname ? ' is-invalid' : '') }/>
-											<ErrorMessage name="lastname" component="div" className="invalid-feedback" />
-									</div>
-								</div>	
-								<div className="form-group">
-									<label htmlFor="emailID" className="mandatory"> Email </label>
-									<Field name="emailID" type="email" className={ 'form-control' + (errors.emailID && touched.emailID ? ' is-invalid' : '') }/>
-									<ErrorMessage name="emailID" component="div" className="invalid-feedback" />
-								</div>
-								<div className="form-group">
-									<label htmlFor="role" className="mandatory">Role</label>
-									<Field as="select" name="role" onChange={handleChange} value={roleVal} type="role" className={ 'form-control' + (errors.role && touched.role ? ' is-invalid' : '') }>
-										<option value="">-- Select option --</option>
-										<option value="Pathologist">Pathologist</option>
-										<option value="Doctor">Doctor</option>
-										<option value="Nurse">Nurse</option>
-									</Field>
-									<ErrorMessage name="role" component="div" className="invalid-feedback" />																																							
-								</div>
-								{showSite && (
-								<div className="form-group">
-									<label htmlFor="site" className="mandatory">Site</label>
-									<Field as="select" name="site" type="site" className={ 'form-control' + (errors.site && touched.site ? ' is-invalid' : '') }>
-										{siteInfo}													
-									</Field>
-									<ErrorMessage name="site" component="div" className="invalid-feedback" />
-								</div>
-								)}
-								<div className="form-group">
-									<label htmlFor="password" className="mandatory"> Password </label>
-									<Field name="password" type="password" className={ 'form-control' + (errors.password && touched.password ? ' is-invalid' : '') }/>
-									<ErrorMessage name="password" component="div" className="invalid-feedback" />
-								</div>
-								<div className="form-group">
-									<label htmlFor="confirmpassword" className="mandatory"> Confirm Password </label>
-									<Field name="confirmpassword" type="password" className={ 'form-control' + (errors.confirmpassword && touched.confirmpassword ? ' is-invalid' : '') }/>
-									<ErrorMessage name="confirmpassword" component="div" className="invalid-feedback" />
-								</div>
-								<div className="form-group pt-2">
-									<div className="custom-checkbox custom-control">
-										<Field type="checkbox" name="acceptTerms" className={'form-check-input ' + (errors.acceptTerms && touched.acceptTerms ? ' is-invalid' : '')} />
-										<label htmlFor="acceptTerms" className="form-check-label">I agree to the <a href="termsandconditions.html">Terms and Conditions </a></label>
-										<ErrorMessage name="acceptTerms" component="div" className="invalid-feedback" />
-									</div>
-								</div>
-								<div className="form-group pt-3 pb-3">
-									<button type="submit" className="btn btn-primary btn-block" id="register">
-										Register
-									</button>
-								</div>
-							</Form>
-						)}
-					</Formik>
-				</div>
-				<div className=" col-12 footer">
-					<strong>iPCM Leaderboard</strong> &copy;2022, All rights reserved.
-				</div>
-			</div>	
-    	</div>
-	<ToastContainer
-		position="top-right"
-		autoClose={5000}
-		hideProgressBar={false}
-		newestOnTop={false}
-		closeOnClick
-		pauseOnFocusLoss
-		draggable
-		pauseOnHover
-		theme="light"
-	/>
-	</>
-    );
+			</div>
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
+		</>
+	);
 }
 
 export default Register;
